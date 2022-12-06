72650c4cc1424f7030139a755bce2ddb0c1acc0a
tknr <geishagirlskoume@gmail.com>
2022-12-06 11:31:07 +0900

    Add files via upload

---
+++ InstagramPlusPlus.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         Instagram++
+// @namespace    tknr.instagram.plusplus
+// @version      2.4.2
+// @updateURL    https://tknr.github.io/InstagramPlusPlus/InstagramPlusPlus.meta.js
+// @downloadURL  https://tknr.github.io/InstagramPlusPlus//InstagramPlusPlus.user.js
+// ==/UserScript==
