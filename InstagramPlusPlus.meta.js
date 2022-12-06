72eaa9a047c9b4d37360cc615a3e80ee1721b3fd
tknr <geishagirlskoume@gmail.com>
2022-12-06 11:34:02 +0900

    Update InstagramPlusPlus.meta.js

--- InstagramPlusPlus.meta.js
+++ InstagramPlusPlus.meta.js
@@ -2,6 +2,6 @@
 // @name         Instagram++
 // @namespace    tknr.instagram.plusplus
 // @version      2.4.2
-// @updateURL    https://tknr.github.io/InstagramPlusPlus/InstagramPlusPlus.meta.js
-// @downloadURL  https://tknr.github.io/InstagramPlusPlus//InstagramPlusPlus.user.js
+// @updateURL    https://tknr.github.io/greasemonkey_scripts/InstagramPlusPlus.meta.js
+// @downloadURL  https://tknr.github.io/greasemonkey_scripts/InstagramPlusPlus.user.js
 // ==/UserScript==
