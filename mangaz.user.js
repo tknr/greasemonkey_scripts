717b7e67fe23bab987718f6b6e4aeafd2a52b920
tknr <geishagirlskoume@gmail.com>
2022-12-06 11:42:34 +0900

    Create mangaz.user.js

---
+++ mangaz.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.mangaz.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mangaz.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+console.log();
+    console.log(w);
+    // Your code here...
+})();
