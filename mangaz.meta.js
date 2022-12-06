cd321cbeab2871225a25b9e5aed42d0b8f80a5c7
tknr <geishagirlskoume@gmail.com>
2022-12-06 11:45:16 +0900

    Create mangaz.meta.js

---
+++ mangaz.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         mangaz
+// @namespace    tknr.mangaz.mangaz
+// @version      0.0.1
+// @updateURL    https://tknr.github.io/greasemonkey_scripts/mangaz.meta.js
+// @downloadURL  https://tknr.github.io/greasemonkey_scripts/mangaz.user.js
+// ==/UserScript==
