4f3c8f615a30d9d42f0d680de95a767ff0987ab9
tknr <geishagirlskoume@gmail.com>
2022-12-06 16:19:21 +0900

    Update mangaz.user.js

--- mangaz.user.js
+++ mangaz.user.js
@@ -6,16 +6,20 @@
 // @author       tknr
 // @homepage     https://tknr.github.io/greasemonkey_scripts/
 // @homepageURL  https://tknr.github.io/greasemonkey_scripts/
-// @match        https://*.mangaz.com/*
+// @match        https://*.mangaz.com/virgo/view/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mangaz.com
 // @updateURL    https://tknr.github.io/greasemonkey_scripts/mangaz.meta.js
 // @downloadURL  https://tknr.github.io/greasemonkey_scripts/mangaz.user.js
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant       GM_addStyle
 // @grant        GM.setValue
 // @grant        GM.getValue
+// @grant       GM.addStyle
 // @grant        GM_registerMenuCommand
+// @run-at      document-idle
 // ==/UserScript==
 
 
@@ -26,27 +30,17 @@
     console.log('mangaz',window.location.href);
     console.dir(window);
 
-addJS_Node (null, null, interceptAjax());
+    var dataUri_1 = getDataUriFromCanvas('.first .page_image canvas');
+    var dataUri_2 = getDataUriFromCanvas('.second .page_image canvas');
+
+    console.dir($('.page_unit .image'));
 
 })();
 
-function interceptAjax () {
-    $(document).ajaxSuccess (
-        function (event, requestData)
-        {
-            console.log (requestData.responseText);
-        }
-    );
-}
-
-function addJS_Node (text, s_URL, funcToRun) {
-    var D                                   = document;
-    var scriptNode                          = D.createElement ('script');
-    scriptNode.type                         = "text/javascript";
-    if (text)       scriptNode.textContent  = text;
-    if (s_URL)      scriptNode.src          = s_URL;
-    if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
-
-    var targ    = D.getElementsByTagName('head')[0] || D.body || D.documentElement;
-    targ.appendChild (scriptNode);
+function getDataUriFromCanvas(elem){
+    var canvas = $(elem)[0];
+    console.dir(canvas);
+    var dataUri = canvas.toDataURL() ;
+    console.log(dataUri)
+    return dataUri;
 }
