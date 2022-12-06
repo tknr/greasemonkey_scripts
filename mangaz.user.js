4c8ca5b5b873afbbf1842fde78279aab9c6593b0
tknr <geishagirlskoume@gmail.com>
2022-12-06 11:45:33 +0900

    Update mangaz.user.js

--- mangaz.user.js
+++ mangaz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mangaz
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.mangaz.com/*
