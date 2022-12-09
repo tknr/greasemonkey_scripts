6743454756b6a88414e89ef11423a975de10cb7c
tk.hirai <tk.hirai@crusis.co.jp>
2022-12-09 15:06:22 +0900

    u

--- fto.user.js
+++ fto.user.js
@@ -4,6 +4,8 @@
 // @version      0.0.1
 // @description  try to take over the world!
 // @author       tknr
+// @homepage     https://tknr.github.io/greasemonkey_scripts/
+// @homepageURL  https://tknr.github.io/greasemonkey_scripts/
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
