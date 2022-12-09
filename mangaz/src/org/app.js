efed62745730cfdc608e6e1fa3dd8003d86d0a76
tk.hirai <tk.hirai@crusis.co.jp>
2022-12-09 16:14:17 +0900

    u

---
+++ mangaz/src/org/app.js
@@ -0,0 +1,7 @@
+(function() {
+ document.open(); var len = arguments.length; for( i = 0; i < len; i++) document.write(arguments[i]); document.close();
+})(
+    '<script>var __serial = "7d9543d0f6ad981f22798ab9eed69426fdcb23c2";</script>',
+    '<script src="/virgo/js/vw6-simple.js?2022110701"></script>',
+    ''
+    );
---
+++ mangaz/src/org/open-viewer.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $(document).on('click', '.open-viewer', function () {
+        let win = window.open($(this).data('url'), $(this).data('target') ? $(this).data('target') : '_viewer');
+        win.focus();
+        return false;
+    });
+    $(document).on('click', '.close-viewer', function () {
+        try {
+            if (window.opener && window.opener.location.hostname === location.hostname) {
+                window.close();
+                return;
+            }
+        } catch (e) {
+        }
+        location.href = url;
+    });
+    $(document).on('click', '.ga', function () {
+        if ($(this).data('ec')) {
+            let el = $(this).data('el') ? $(this).data('el') : $(this).text();
+            if (el) {
+                gtag('event', 'click', {
+                    'event_category': $(this).data('ec'),
+                    'event_label': el
+                });
+            }
+        }
+    });
+    $(document).on('click', '.ga-text', function () {
+        if ($(this).data('ec')) {
+            let el = $(this).data('el') ? $(this).data('el') + $(this).text() : $(this).text();
+            if (el) {
+                gtag('event', 'click', {
+                    'event_category': $(this).data('ec'),
+                    'event_label': el
+                });
+            }
+        }
+    });
+    /*
+    $(document).on('click', '.premium-link', function () {
+        if ($(this).data('el')) {
+            gtag('event', 'click', {
+                'event_category': $(this).data('category') ? $(this).data('category') : 'premium-link',
+                'event_label': ($(this).data('el'))
+            });
+        }
+    });
+    */
+});
