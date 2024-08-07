0a9bea495d5516ce6f34980a31ddc2e77eca9dac
tknr <geishagirlskoume@gmail.com>
2024-08-07 22:15:41 +0900

    Create nicop.user.js

---
+++ nicovideo/nicop.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-08-07
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.nicovideo.jp/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nicovideo.jp
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    // <meta name="server-response" content=
+    // &quot;isPremium&quot;:false,
+})();
